9dfb3320c7060b347a1c16442241c675ebced5aa
kwdseymour <62351151+kwdseymour@users.noreply.github.com>
2021-01-22 17:56:06 +0100

    Change wind preprocessing to prioritize FLh
    Add functionality to select wind turbines at each location by optimizing different metrics: LCOE, FLh, or density. Set the default to FLh. Add on_off_shore.csv file to indicate variants of turbines.

--- scripts/02_merra_wind_preprocessing/01_merra_wind_preprocessing.py
+++ scripts/02_merra_wind_preprocessing/01_merra_wind_preprocessing.py
@@ -1,5 +1,3 @@
-#!~/anaconda3/envs/GIS/bin/python
-
 #bsub -n 32 -R "rusage[mem=4096]" -oo $HOME/SAFlogistics/results/01_merra_wind_preprocessing 'python SAFlogistics/scripts/01_merra_wind_preprocessing/01_merra_wind_preprocessing.py -d $HOME/SAFlogistics -p32'
 
 '''
@@ -11,7 +9,7 @@ Opening of the MERRA files for some countries can require memory on the order of
 - evaluation countries: the list of countries for which the MERRA files is to be processed must be contained in a CSV file in the same directory as this script
 
 - evaluation points: the JSON file containing the points that should be evaluated within each country should be in the following path relative to the "SAFlogistics" directory:
-    "/data/Countries_WGS84/Europe_Evaluation_Points.json"
+    "/data/Countries_WGS84/processed/Europe_Evaluation_Points.json"
 
 - MERRA files: these netCDF4 files must be contained in folders named according to the respsective country in the following path relative to the "SAFlogistics" directory:
     "/data/MERRA/"
@@ -52,6 +50,12 @@ parser.add_argument( # necessary input of user
     help='The path to the "SAFlogistics" directory',
 )
 parser.add_argument( # optional input of user
+    '-m','--optimization_metric',
+    help='The metric by which the turbine selection should be optimized. Must be one of the following: "lcoe", "flh", or "density".',
+    default="flh",
+    type=str
+)
+parser.add_argument( # optional input of user
     '-p','--max_processes',
     help='The maximum number of subprocesses to use for executing the file operations.',
     default=12,
@@ -61,6 +65,7 @@ args = parser.parse_args()
 
 max_processes = args.max_processes
 SAF_directory = args.SAF_directory
+optimization_metric = args.optimization_metric
 
 # Define path to logs, cache, and results folders. Create directories if they don't already exist
 logs_path = os.path.join(SAF_directory,'logs')
@@ -77,26 +82,10 @@ if not os.path.isdir(results_path):
 sys.path.insert(1, os.path.join(SAF_directory,'scripts','03_plant_optimization'))
 from plant_optimization.plant_optimizer import Component,costs_NPV
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(process)d - %(levelname)s: %(message)s')
-# File handler directed to a persistent log that is appended to.
-fh1 = logging.FileHandler(os.path.join(logs_path,f'{script_name}_persistent.log'))
-fh1.setLevel(logging.INFO)
-fh1.setFormatter(formatter)
-# File handler directed to a log that is overwritten with every script run.
-fh2 = logging.FileHandler(os.path.join(logs_path,f'{script_name}.log'),mode='w')
-fh2.setLevel(logging.DEBUG)
-fh2.setFormatter(formatter)
 
-ch = logging.StreamHandler()
-ch.setLevel(logging.ERROR)
-ch.setFormatter(formatter)
-
-logger.addHandler(fh1)
-logger.addHandler(fh2)
-logger.addHandler(ch)
-logger.propagate = False
+# Add a logger
+from plant_optimization.utilities import create_logger
+logger = create_logger(SAF_directory,__name__,__file__)
 
 # Validate countries in config file. Remove countries not found in the MERRA folder
 countries_filepath = os.path.join(os.path.dirname(sys.argv[0]),f'{script_name}.csv')
@@ -105,7 +94,6 @@ try:
     countries = list(pd.read_csv(countries_filepath, index_col=0)['country'])
 except FileNotFoundError:
     raise Exception('Configuration file not found. The csv file containing the list of coutries to analyze should be found in the same directory location as this script.')
-
 removed_countries = []
 for country in countries:
     if country not in str(os.listdir(os.path.join(SAF_directory,'data','MERRA'))):
@@ -117,12 +105,10 @@ logger.info('Country set used for analysis: {}'.format(countries))
 
 # Read evaluation points file used to filter out points which are without country borders
 try:
-    with open(os.path.join(SAF_directory,'data','Countries_WGS84/Europe_Evaluation_Points.json'),'r') as fp:
-        eu_eval_points = json.load(fp)
-    with open(os.path.join(SAF_directory,'data','Countries_WGS84/Coast_Evaluation_Points.json'),'r') as fp:
-        coast_eval_points = json.load(fp)
+    with open(os.path.join(SAF_directory,'data','Countries_WGS84/processed/Europe_Evaluation_Points.json'),'r') as fp:
+        eval_points = json.load(fp)
 except FileNotFoundError:
-    raise Exception('Europe_Evaluation_Points.json file not found. This file containing the MERRA points within each country\'s borders must be available at data/Countries_WGS84/.')
+    raise Exception('Europe_Evaluation_Points.json file not found. This file containing the MERRA points within each country\'s borders must be available at data/Countries_WGS84/processed/.')
 
 def extract_date(data_set):
     """
@@ -157,12 +143,9 @@ def extract_date(data_set):
         return data_set
         
 
-def files_to_dataframe(country, coast):
+def files_to_dataframe(country):
     '''Extracts all files in the country folder and concatenates them by date into a DataFrame.'''
-    if coast:
-        file_path = os.path.join(SAF_directory, 'data', 'MERRA', country,'coast', '*.nc4')
-    else:
-        file_path = os.path.join(SAF_directory,'data','MERRA',country,'*.nc4')
+    file_path = os.path.join(SAF_directory,'data','MERRA',country, '*.nc4')
     try:
         # with xr.open_mfdataset(file_path, concat_dim='date', combine='nested',preprocess=extract_date) as ds_wind:
         with xr.open_mfdataset(file_path, concat_dim='lat', combine='by_coords') as ds_wind:
@@ -181,7 +164,7 @@ def files_to_dataframe(country, coast):
         logger.info('{}: {} NaN rows were found and dropped.'.format(country,init_len - len(df_wind)))
     return df_wind
 
-def preprocess_df(df_wind,country,eval_points):
+def preprocess_df(df_wind,country):
     '''Resets and configures multi-index of dataframe, calculates wind velocities, and calculates Hellmann exponent.'''
     df_wind.reset_index(inplace=True)
     df_wind.loc[abs(df_wind.lon)<1e-3,'lon'] = 0 # xarray reads zeros as very small numbers, this sets them correctly
@@ -212,14 +195,23 @@ turbine_classes = {}
 turbine_cost_objects = {}
 specs_path = os.path.join(SAF_directory,'data','plant_assumptions.xlsx')
 component_specs = pd.read_excel(specs_path,sheet_name='data',index_col=0)
+# turbine_specs = wpl.wind_turbine.load_turbine_data_from_oedb()
 turbine_specs = pd.read_csv(os.path.join(os.path.dirname(wpl.__file__),'oedb','turbine_data.csv'))
 all_types = list(wpl.wind_turbine.get_turbine_types('local',filter_=True,print_out=False)['turbine_type'].unique())
+on_off_shore = pd.read_csv(os.path.join(os.path.dirname(sys.argv[0]),'on_off_shore.csv')) # Reads a csv that indicates whether each turbine model have on- and off-shore variants
+on_shore_names = list(on_off_shore.loc[on_off_shore.on_shore==1,'turbine_type'])
+off_shore_names = list(on_off_shore.loc[on_off_shore.off_shore==1,'turbine_type'])
+on_shore_types = [x for x in all_types if x in on_shore_names]
+off_shore_types = [x for x in all_types if x in off_shore_names]
+print([x for x in on_off_shore.turbine_type])
+missing_shore_designation = [x for x in all_types if x not in on_off_shore.turbine_type]
+if len(missing_shore_designation) > 0:
+    logger.error(f'The following witn turbine types were not assigned on- or off-shore designationsin the on_off_shore.csv file. They are therefore not being used: {missing_shore_designation}')
 specific_capacity_classes = {0.2:'lo',0.3:'mid',0.47:'hi'} # 2018 JRC report wind turbing specific capacity classes used for determining costs
 rep_specific_capacities = [0.2,0.3,0.47] # 2018 JRC report wind turbine class representative specific capacities
 rep_hub_heights = {0.2:200, 0.3:100, 0.47:50} # 2018 JRC report wind turbine class hub heights for each of the three representative specific capacities
-loading_errors = []
-
-for model in all_types:
+loading_errors = {}
+for model in on_shore_types:
     '''
     Select the hub that matches most closely to the closest wind turbine category in the 2018 JRC report:
      - Turbine specific capacity of 0.2 kW/m2 (low specific capacity) and at 200 m hub height (high hub height)
@@ -228,67 +220,58 @@ for model in all_types:
     '''
     try:
         specific_capacity = turbine_specs.loc[turbine_specs.turbine_type==model,'nominal_power'].item()/1e3/turbine_specs.loc[turbine_specs.turbine_type==model,'rotor_area'].item() # [kW/m2]
-        hub_height_item = turbine_specs.loc[turbine_specs.turbine_type==model,'hub_height'].item().replace(' ','')
+        hub_height_item = str(turbine_specs.loc[turbine_specs.turbine_type==model,'hub_height'].item()).replace(' ','')
         hub_height_list = [float(x) for x in re.split(';|/|,',hub_height_item) if len(x)>0]
         rep_specific_capacity = min(rep_specific_capacities, key=lambda x:abs(x-specific_capacity)) # Selects the JRC representative specific capacity closest to that of this turbine
         rep_hub_height = rep_hub_heights[rep_specific_capacity] # Selects the representative hub height for the JRC wind turbine class closets to this turbine's specific capacity
         hub_height = min(hub_height_list, key=lambda x:abs(x-rep_hub_height)) # [m] Selects the hub height for this model that is closest to the representative hub height found above
         turbines[model] = wpl.wind_turbine.WindTurbine(turbine_type=model,hub_height=hub_height)
         turbine_classes[model] = specific_capacity_classes[rep_specific_capacity]
-        if model in ['V164/8000', 'S152/6330']:
-            turbine_cost_objects[model] = Component('wind',specs=component_specs,wind_class='off')
-        else:
-            turbine_cost_objects[model] = Component('wind', specs=component_specs, wind_class='on')
-    except:
-        loading_errors.append(model)
-
+        turbine_cost_objects[model] = Component('wind',specs=component_specs,wind_class=specific_capacity_classes[rep_specific_capacity])
+    except Exception as e:
+        loading_errors[model] = e
 if len(loading_errors)>0:
-    logger.info('There was a problem loading the following wind turbine models: {}'.format(str(loading_errors)))
+    logger.info('There was a problem loading the following wind turbine models: {}'.format(str(loading_errors.keys())))
+logger.info('The following wind turbine models were properly loaded: {}'.format(str(turbines.keys())))
 
-#turbine_spacing_on = component_specs.loc[component_specs.index=='windon_turbine_spacing']['value'].values[0]
-#turbine_spacing_off = component_specs.loc[component_specs.index=='windoff_turbine_spacing']['value'].values[0]
+turbine_spacing = 5 # meters of spacing per meter of rotor diameter [Bryer, 2012]
 models=[]
 specific_outputs=[]
 
-def assign_turbine_model(df,coast,type):
-    '''Calculates hypothetical power output for each turbine model in "models" and returns the model with the highest yearly output per land area usage.'''
-    if coast:
-        for name in list(turbines.keys()):
-            if name not in['V164/8000', 'S152/6330']:
-                turbines.pop(name)
-    else:
-        for name in list(turbines.keys()):
-            if name in ['V164/8000', 'S152/6330']:
-                turbines.pop(name)
+def assign_turbine_model(df,optimization_metric='lcoe'):
+    '''Calculates hypothetical power output for each turbine model in "models" and returns the model with the most optimal optimization metric value.
+    
+    Optimization can be performed with any one the following "optimization_metrics":
+    "lcoe": levelized cost of electricity 
+    "flh": full load hours
+    "density": the power production density measured in kWh per land area required for the turbine
+    '''
     winner_model = ''
-    winner_score = 0
+    winning_value = 0
     for name,turbine in turbines.items():
         speed_at_hub = df.v_50m*(turbine.hub_height/50)**df.hellmann
         output = sum(wpl.power_output.power_curve(speed_at_hub,turbine.power_curve.wind_speed,turbine.power_curve.value))/1e3 #kWh
-        pos_output = turbine.nominal_power/1e3 * (366*24) #kWh
-        share = output/pos_output
         cost_object = turbine_cost_objects[name]
-        if coast:
-            CAPEX = cost_object.CAPEX
-            OPEX = cost_object.OPEX
-            lifetime=cost_object.lifetime
+        if optimization_metric.lower() == 'lcoe':
+            metric = costs_NPV(capex=cost_object.on_CAPEX, opex=cost_object.on_OPEX,
+                                discount_rate=component_specs.at['discount_rate', 'value'], lifetime=cost_object.on_lifetime,
+                                capacity=turbine.nominal_power / 1e3) / np.sum([output/(1+component_specs.at['discount_rate', 'value'])**n for n in np.arange(cost_object.on_lifetime+1)])  # EUR/kWh
+        elif optimization_metric.lower() == 'flh':
+            metric = output/(turbine.power_curve.value.max()/1e3)
+        elif optimization_metric.lower() == 'density':
+            turbine_area = (turbine.rotor_diameter*turbine_spacing)**2 # [m^2]
+            metric = output/turbine_area #kWh/m2
         else:
-            CAPEX = cost_object.CAPEX
-            OPEX = cost_object.OPEX
-            lifetime = cost_object.lifetime
-        lcoe = costs_NPV(capex=CAPEX, opex=OPEX,discount_rate=component_specs.at['eco_discount_rate', 'value'], lifetime=lifetime,capacity=turbine.nominal_power / 1e3) / (output * cost_object.lifetime)  # EUR/kWh
-        if type == 'cost':
-            if lcoe > winner_score:
-                winner_model = name
-                winner_score = lcoe
-        else:
-            if share > winner_score:
-                winner_model = name
-                winner_score = share
-
+            logger.error('Invalid wind turbine optimization_metric. Must be "lcoe", "flh", or "density".')
+            sys.exit()
+        if metric > winning_value:
+            winner_model = name
+            winning_value = metric
+    if winner_model == '':
+        logger.error(f'Problem assigning a turbine model {df}. Latest metric value {metric}.')
     return winner_model
         
-def compute_power_output(df, coast,type):
+def compute_power_output(df):
     '''Calculates the hourly power output from the wind speed data using an optimal wind turbine.
 
     - The optimal wind turbine is derived from the "assign_turbine_model" function.
@@ -301,7 +284,7 @@ def compute_power_output(df, coast,type):
     df['rated_power_MW'] = ''
     df['specific_capacity_class'] = ''
     for coords in df.index.droplevel(2).unique():
-        optimal_turbine = assign_turbine_model(df.loc[coords], coast,type)
+        optimal_turbine = assign_turbine_model(df.loc[coords],optimization_metric=optimization_metric)
         speed_at_hub = df.loc[coords].v_50m*(turbines[optimal_turbine].hub_height/50)**df.loc[coords].hellmann
         output = wpl.power_output.power_curve(speed_at_hub,turbines[optimal_turbine].power_curve.wind_speed,turbines[optimal_turbine].power_curve.value) # [Wh]
         df.loc[idx[coords],'kWh'] = list(output/1e3) # [kWh]
@@ -310,52 +293,41 @@ def compute_power_output(df, coast,type):
         df.loc[idx[coords],'rotor_diameter'] = turbines[optimal_turbine].rotor_diameter # [m]
         df.loc[idx[coords],'specific_capacity_class'] = turbine_classes[optimal_turbine]
 
-def process_country(country, eval_points, coast, type):
+def process_country(country, coast):
     '''Extract files to DataFrame, preprocess dataframe, and compute power output for each country in the "countries" list'''
     # Create a script_name for caching the interim results: "01_merra_wind_preprocessing_<country>_<script_time>.csv"
     logger.info(f'{country} processing started...')
     cache_file_name = f'{script_name}_{country}_{sstime_string}.csv'
     # Extract files & preprocess DataFrame
-    df_wind = files_to_dataframe(country, coast)
-    preprocess_df(df_wind,country,eval_points)
+    df_wind = files_to_dataframe(country)
+    preprocess_df(df_wind,country)
     # Cache DatFrame
     logger.info(f'Initial caching for {country}...')
     df_wind.to_csv(os.path.join(cache_path,cache_file_name))
     logger.info(f'Initial {country} cache saved')
     # Calculate power output
-    compute_power_output(df_wind, coast,type)
+    compute_power_output(df_wind)
     # Cache & save results
     logger.info(f'Saving results for {country}...')
     df_wind.to_csv(os.path.join(cache_path,cache_file_name))
-    if type == 'cost':
-        if coast:
-            df_wind.to_parquet(os.path.join(results_path,'optimal_cost', 'coast', f'{country}.parquet.gzip'), compression='gzip')
-            df_wind.to_csv(os.path.join(results_path,'optimal_cost','coast', f'{country}.csv'))
-        else:
-            df_wind.to_parquet(os.path.join(results_path,'optimal_cost',f'{country}.parquet.gzip'),compression='gzip')
-            df_wind.to_csv(os.path.join(results_path,'optimal_cost',f'{country}.csv'))
-    else:
-        if coast:
-            df_wind.to_parquet(os.path.join(results_path,'optimal_out', 'coast', f'{country}.parquet.gzip'), compression='gzip')
-            df_wind.to_csv(os.path.join(results_path,'optimal_out','coast', f'{country}.csv'))
-        else:
-            df_wind.to_parquet(os.path.join(results_path,'optimal_out',f'{country}.parquet.gzip'),compression='gzip')
-            df_wind.to_csv(os.path.join(results_path,'optimal_out',f'{country}.csv'))
-
+    df_wind.to_parquet(os.path.join(results_path,f'{country}.parquet.gzip'),compression='gzip')
+    df_wind.to_csv(os.path.join(results_path,f'{country}.csv'))
     logger.info(f'Results for {country} saved')
 
-countries_eu = list(eu_eval_points.keys())
+    if coast == True:
+        if os.path.isdir('./data/MERRA/' + country + '/coast'):
+            print(country)
+        else:
+            pass
+            #df_wind = files_to_dataframe(country)
+            #preprocess_df(df_wind, country)
+            #df_wind.to_csv(os.path.join(cache_path, cache_file_name))
+            #compute_power_output(df_wind)
+            # df_wind.to_parquet(os.path.join(results_path,f'{country}.parquet.gzip'),compression='gzip')
 
-#for country in countries_eu:
-    #print(country)
-    #process_country(country, eu_eval_points, coast = False, type = 'cost')
-    #process_country(country, eu_eval_points, coast=False, type='out')
+coast = True
 
-countries_coast = list(coast_eval_points.keys())[22:]
-
-for country in countries_coast:
-    print(country)
-    process_country(country, coast_eval_points, coast = True, type = 'cost')
-    process_country(country, coast_eval_points, coast=True, type='out')
+for country in countries:
+    process_country(country, coast)
 
 logger.info('Script time: {:.2f} seconds.'.format(time.time()-sstime))
