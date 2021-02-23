82493905688df92064d07de1c4c3a75f172f70a3
kwdseymour <62351151+kwdseymour@users.noreply.github.com>
2021-02-23 10:55:48 +0100

    Create Python script to submit batch jobs
    Instead of using a bash script, the Python script will submit the jobs on Euler. This adds the functionality to be able to divide the jobs into custom sizes to optimize runtime.

--- scripts/03_plant_optimization/02_plant_optimization.py
+++ scripts/03_plant_optimization/02_plant_optimization.py
@@ -56,13 +56,16 @@ parser.add_argument('-d','--SAF_directory',
 parser.add_argument('-p','--max_processes',
     help='Controls the number of threads to apply to parallel algorithms (of the optimizer)',
     default=None)
+parser.add_argument('-n','--bin_size',
+    help='The maximum number of points that will be evaluated for the given country.',
+    type=int)
 parser.add_argument('-b','--bin_number',
-    help='Chooses which coordinates from the country to evaluate',
+    help='Indicates which bin of coordinates from the country to evaluate',
     default=1,
     type=int)
 parser.add_argument('-t','--time_limit',
     help='Sets the time limit of the optimizer (seconds)',
-    default=2000,
+    default=3000,
     type=int)
 parser.add_argument('-m','--MIPGap',
     help='Sets the MIP gap of the optimizer',
@@ -93,7 +96,8 @@ save_operation = args.save_operation
 timelimit = args.time_limit
 bin_number = args.bin_number
 offshore = args.offshore
-bin_size = 100
+onshore = not offshore
+bin_size = args.bin_size
 country = args.country
 max_processes = args.max_processes
 SAF_directory = args.SAF_directory
@@ -110,6 +114,8 @@ cache_path = os.path.join(scratch_path,'cache')
 if not os.path.isdir(cache_path):
     os.mkdir(cache_path)
 results_path = os.path.join(SAF_directory,'results',script_name)
+if offshore:
+    results_path = os.path.join(results_path,'offshore')
 if not os.path.isdir(results_path):
     os.mkdir(results_path)
 if save_operation:
@@ -128,26 +134,38 @@ popt_logger = logging.getLogger('plant_optimization.plant_optimizer')
 popt_logger.addHandler(logger.handlers[0])
 popt_logger.addHandler(logger.handlers[1])
 
-if os.path.isfile(os.path.join(results_path,country+'.csv')):
-    logger.error(f'{country} results already found in results folder. Remove file in order to perform new analysis.')
-    sys.exit()
-
 cores_avail = multiprocessing.cpu_count()
 logger.info(f'{cores_avail} cores available')
 if max_processes == None:
     max_processes = cores_avail - 1
     logger.info(f'Max processes parameter not set. Using {max_processes} cores.')
 
-results_df = pd.DataFrame()
-points = pop.get_country_points(country,onshore=not onshore,offshore=offshore)
+europe_points = pd.read_csv(os.path.join(SAF_directory,'data/Countries_WGS84/processed/Europe_Evaluation_Points.csv'),index_col=0)
+points = europe_points.loc[europe_points.country==country].set_index(['grid_lat','grid_lon'])
+if onshore and not offshore:
+    points = points.loc[~points.sea_node]
+elif not onshore and offshore:
+    points = points.loc[points.sea_node]
+elif onshore and offshore:
+    pass
+else:
+    print('Either onshore or offshore must be set to TRUE')
+points = list(points.index.unique())
+
 bin_count = int(np.ceil(len(points)/bin_size))
 bin_string = f'_{bin_number}-{bin_count}'
 points = points[(bin_number-1)*bin_size:bin_number*bin_size]
+
+if os.path.isfile(os.path.join(results_path,country+bin_string+'.csv')):
+    logger.error(f'{country} results already found in results folder. Remove file in order to perform new analysis.')
+    sys.exit()
+
+results_df = pd.DataFrame()
 for i,point in enumerate(points):
     # with open(eval_points_path,'r') as fp:
     #     eval_points_str = fp.read()
     # if str(point) not in eval_points_str:
-    logger.info(f'Starting {country} point {i} of {len(points)}.')
+    logger.info(f'Starting {country} point {i+1} of {len(points)}.')
     try:
         site = pop.Site(point,country,offshore=offshore)
         plant = pop.Plant(site)
@@ -156,13 +174,13 @@ for i,point in enumerate(points):
             pop.unpack_design_solution(plant, unpack_operation=True)
         except:
             logger.error(f'There was a problem unpacking the optimizer model for point({point}) in {country}.')
-            raise Exception('1')
+            continue
     except pop.errors.CoordinateError:
         logger.error(f'Coordinate error for point ({point}) in {country}.')
         continue
     except:
         logger.error(f'Optimization problem for point ({point}) in {country}.')
-        raise Exception('2')
+        continue
     try:
         results_dict = pop.solution_dict(plant)
         with open(eval_points_path,'a') as fp:
@@ -190,10 +208,6 @@ results_df = results_df[['lat','lon','shore_designation','turbine_type','rotor_d
 'battery_capacity_MWh','H2stor_capacity_MWh','CO2stor_capacity_ton','H2tL_capacity_MW','curtailed_el_MWh',
 'wind_production_MWh','PV_production_MWh','NPV_EUR','CAPEX_EUR','LCOF_MWh','LCOF_liter','runtime']]
 
-if offshore:
-    results_path = os.path.join(results_path,'offshore')
-    if not os.path.isdir(results_path):
-        os.mkdir(results_path)
 results_df.to_csv(os.path.join(results_path,country+bin_string+'.csv'))
 
 logger.info(f'Script finished after {time.time()-sstime:.1f} seconds.')
---
+++ scripts/03_plant_optimization/02_plant_optimization_run.py
@@ -0,0 +1,81 @@
+# python scripts/03_plant_optimization/02_plant_optimization_run.py
+# python scripts/03_plant_optimization/02_plant_optimization_run.py -o
+
+import os
+import sys
+import numpy as np
+import argparse
+import time
+import pandas as pd
+
+desc_text = 'This script submits jobs to run all optimizations for all countries.'
+parser = argparse.ArgumentParser(description=desc_text)
+parser.add_argument('-d','--SAF_directory',
+    help='The path to the "SAFlogistics" directory',
+    default = None,
+    type=str)
+parser.add_argument('-o','--offshore',
+    action='store_true',
+    help='Calculate offshore points.',
+    default=False)
+parser.add_argument('-n','--bin_size',
+    help='The maximum number of points that will be evaluated for each job.',
+    default=50,
+    type=int)
+args = parser.parse_args()
+
+SAF_directory = args.SAF_directory
+offshore = args.offshore
+onshore = not offshore
+bin_size = args.bin_size
+
+cores = 32
+wall_time = '30:00'
+# cores = 8
+# wall_time = '3:00'
+MIPGap = 0.01
+DisplayInterval = 30
+if SAF_directory == None:
+    SAF_directory = os.environ['HOME']
+    SAF_directory = os.path.join(SAF_directory,'EuroSAFs')
+results_path = os.path.join(SAF_directory,'results','02_plant_optimization')
+if offshore:
+    results_path = os.path.join(results_path,'offshore')
+    offshore_flag = '-o'
+else:
+    offshore_flag = ''
+
+europe_points = pd.read_csv(os.path.join(SAF_directory,'data/Countries_WGS84/processed/Europe_Evaluation_Points.csv'),index_col=0)
+countries = europe_points.country.unique()
+# countries = ['Spain']
+
+
+# loop through the countries list: 
+for country in countries:
+    points = europe_points.loc[europe_points.country==country].set_index(['grid_lat','grid_lon'])
+    if onshore and not offshore:
+        points = points.loc[~points.sea_node]
+    elif not onshore and offshore:
+        points = points.loc[points.sea_node]
+    elif onshore and offshore:
+        pass
+    else:
+        print('Either onshore or offshore must be set to TRUE')
+    points = list(points.index.unique())
+
+    bins = int(np.ceil(len(points)/bin_size))
+    
+    
+    for i in range(bins):
+        i+=1
+        bin_string = f'_{i}-{bins}'
+        file_path = os.path.join(results_path,country+bin_string+'.csv')
+        if os.path.isfile(file_path):
+            print(f'{file_path} already exists. Delete file to run this set.')
+            continue
+            
+        bash_str = f'bsub -n {cores} -W {wall_time} -J "{country}-{i}" -oo {results_path}/lsf.{country}-{i}.txt python $HOME/EuroSAFs/scripts/03_plant_optimization/02_plant_optimization.py -d $HOME/EuroSAFs -c {country} -m {MIPGap} -i {DisplayInterval} -b {i} -n {bin_size} -v -s {offshore_flag}'
+        # bash_str = f'python $HOME/EuroSAFs/scripts/03_plant_optimization/02_plant_optimization.py -d $HOME/EuroSAFs -c {country} -m {MIPGap} -i {DisplayInterval} -b {i} -n {bin_size} -v -s'
+        # bash_str = f'python $HOME/GitHub/EuroSAFs/scripts/03_plant_optimization/02_plant_optimization.py -d $HOME/GitHub/EuroSAFs -c {country} -m {MIPGap} -i {DisplayInterval} -b {i} -n {bin_size} -v -s'
+        os.system(bash_str)
+        time.sleep(0.1)
