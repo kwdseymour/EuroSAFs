fbe965d283510170e3db48763bd09fdcb397123b
kwdseymour <62351151+kwdseymour@users.noreply.github.com>
2021-03-05 12:33:12 +0100

    Update utilities.py
    Error avoidance

--- scripts/03_plant_optimization/plant_optimization/utilities.py
+++ scripts/03_plant_optimization/plant_optimization/utilities.py
@@ -8,6 +8,8 @@ def create_logger(scratch_path,name,file):
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(process)d - %(levelname)s: %(message)s','%Y-%m-%d %H:%M:%S')
+    if not os.path.isdir(os.path.join(scratch_path,'logs')):
+        os.mkdir(os.path.join(scratch_path,'logs'))
     file_handler1 = logging.FileHandler(os.path.join(scratch_path,'logs',f'{file_string}_persistent.log'))
     file_handler1.setLevel(logging.INFO)
     file_handler1.setFormatter(formatter)
