be82d7ab96bee0f76c78fa8db48074c333c3d73e
kwdseymour <62351151+kwdseymour@users.noreply.github.com>
2021-03-14 17:56:36 +0100

    Document plant_optimization_run scripts
    Add more documentation.

--- scripts/optimization/plant_optimization_sensitivity_run.py
+++ scripts/optimization/plant_optimization_sensitivity_run.py
@@ -1,5 +1,5 @@
-# python scripts/optimization/sensitivity_plant_optimization_run.py -d . -n 1 -s 10
-# python scripts/optimization/sensitivity_plant_optimization_run.py -d $HOME/EuroSAFs -s 1000
+# python scripts/optimization/plant_optimization_sensitivity_run.py -d . -n 1 -s 10
+# python scripts/optimization/plant_optimization_sensitivity_run.py -d $HOME/EuroSAFs -s 1000
 
 import os
 import sys
@@ -36,44 +36,62 @@ onshore = not offshore
 bin_size = args.bin_size
 sample_size = args.sample_size
 
-cores = 32
-wall_time = '30:00'
-MIPGap = 0.01
-DisplayInterval = 30
+# Set job resource requirements
+cores = 32 # Number of cores requested for each job
+wall_time = '30:00' # Wall time requested for each job
 
+# Set optimizer parameters
+MIPGap = 0.01
+DisplayInterval = 30 # This sets how frequently Gurobi prints the optimizer progress
+
+# Define path to primary directory
 if SAF_directory == None:
     SAF_directory = os.environ['HOME']
     SAF_directory = os.path.join(SAF_directory,'EuroSAFs')
+
+# Retrieves the SHA code of the current git commit. This is used for file handling
 try:
     repo = git.Repo(search_parent_directories=True)
     git_sha = repo.head.object.hexsha
     git_sha_str = git_sha[:7]+'_'
 except:
     git_sha_str = ''
-results_path = os.path.join(SAF_directory,'results','plant_optimization',git_sha_str+'sensitivity')
 
+
+# Define the results path. The final results folder will contain the git SHA code plus the given evalutation year
+results_path = os.path.join(SAF_directory,'results','plant_optimization',git_sha_str+'sensitivity')
+# Further identify results path by oshore/offshore
 if offshore:
     results_path = os.path.join(results_path,'offshore')
     offshore_flag = '--offshore'
 else:
     results_path = os.path.join(results_path,'onshore')
     offshore_flag = ''
+# Create the directory if it doesn't already exist
 if not os.path.isdir(results_path):
     os.makedirs(results_path)
 
+# Get all evaluation points
 europe_points = pd.read_csv(os.path.join(SAF_directory,'data/Countries_WGS84/processed/Europe_Evaluation_Points.csv'),index_col=0)
 points_set = europe_points.loc[europe_points.sea_node==offshore]
 
+# Randomly select evaluation points
 rand_idxs = random.sample(list(points_set.index),sample_size)
 eval_points = points_set.loc[rand_idxs].sort_index()
+
+# Save randomly selected points to CSV file to be read by plant_optimization.py script
 eval_points.to_csv(os.path.join(results_path,'eval_points.csv'))
 points = list(eval_points.index.unique())
 time.sleep(1)
 
-# loop through the countries list: 
+# Divide the points list into a set of bins depending on the bin_size given as a script argument. (Default=50)
 bins = int(np.ceil(len(eval_points)/bin_size))
+
+# loop through the countries list: 
 for i in range(bins):
     i+=1
+
+    # Generate the job submission string to be submitted to Euler
     bash_str = f'bsub -n {cores} -W {wall_time} -J "sensitivity-{i}" -oo {results_path}/lsf.sensitivity-{i}.txt '\
             f'python {SAF_directory}/scripts/optimization/plant_optimization.py '\
             f'--SAF_directory {SAF_directory} '\
@@ -88,5 +106,7 @@ for i in range(bins):
             f'--save_operation '\
             f'--verbose '\
             f'--sensitivity_analysis'
+    
+    # Execute the generated job submission bash command 
     os.system(bash_str)
     time.sleep(0.01)
--- scripts/optimization/run_plant_optimization.py
+++ scripts/optimization/run_plant_optimization.py
@@ -1,5 +1,5 @@
-# python scripts/optimization/plant_optimization_run.py
-# python scripts/optimization/plant_optimization_run.py -o
+# python scripts/optimization/run_plant_optimization.py
+# python scripts/optimization/run_plant_optimization.py -o
 
 import os
 import sys
@@ -9,7 +9,7 @@ import time
 import pandas as pd
 import git
 
-desc_text = 'This script submits jobs to run all optimizations for all countries.'
+desc_text = 'This script submits jobs to Euler to run all optimizations for all countries.'
 parser = argparse.ArgumentParser(description=desc_text)
 parser.add_argument('-d','--SAF_directory',
     help='The path to the "SAFlogistics" directory',
@@ -35,32 +35,41 @@ onshore = not offshore
 year = args.year
 bin_size = args.bin_size
 
-cores = 32
-wall_time = '30:00'
-# cores = 8
-# wall_time = '3:00'
+# Set job resource requirements
+cores = 32 # Number of cores requested for each job
+wall_time = '30:00' # Wall time requested for each job
+
+# Set optimizer parameters
 MIPGap = 0.01
-DisplayInterval = 30
+DisplayInterval = 30 # This sets how frequently Gurobi prints the optimizer progress
+
+# Define path to primary directory
 if SAF_directory == None:
     SAF_directory = os.environ['HOME']
     SAF_directory = os.path.join(SAF_directory,'EuroSAFs')
+
+# Retrieves the SHA code of the current git commit. This is used for file handling
 try:
     repo = git.Repo(search_parent_directories=True)
     git_sha = repo.head.object.hexsha
     git_sha_str = git_sha[:7]+'_'
 except:
     git_sha_str = ''
-results_path = os.path.join(SAF_directory,'results','plant_optimization',git_sha_str+str(year))
 
+# Define the results path. The final results folder will contain the git SHA code plus the given evalutation year
+results_path = os.path.join(SAF_directory,'results','plant_optimization',git_sha_str+str(year))
+# Further identify results path by oshore/offshore
 if offshore:
     results_path = os.path.join(results_path,'offshore')
     offshore_flag = '--offshore'
 else:
     results_path = os.path.join(results_path,'onshore')
     offshore_flag = ''
+# Create the directory if it doesn't already exist
 if not os.path.isdir(results_path):
     os.makedirs(results_path)
 
+# Get all evaluation points
 europe_points = pd.read_csv(os.path.join(SAF_directory,'data/Countries_WGS84/processed/Europe_Evaluation_Points.csv'),index_col=0)
 countries = europe_points.country.unique()
 # countries = ['Spain']
@@ -68,6 +77,7 @@ countries = europe_points.country.unique()
 
 # loop through the countries list: 
 for country in countries:
+    # Subset the points according to onshore/offshore
     points = europe_points.loc[europe_points.country==country].set_index(['grid_lat','grid_lon'])
     if onshore and not offshore:
         points = points.loc[~points.sea_node]
@@ -77,19 +87,22 @@ for country in countries:
         pass
     else:
         print('Either onshore or offshore must be set to TRUE')
+    # Assign points to list
     points = list(points.index.unique())
 
+    # Divide the points list into a set of bins depending on the bin_size given as a script argument. (Default=50)
     bins = int(np.ceil(len(points)/bin_size))
-    
-    
-    for i in range(bins):
+    for i in range(bins): # Loop thourgh points bins
         i+=1
         bin_string = f'_{i}-{bins}'
+
+        # Check if the result already exists and skip the bin if it does
         file_path = os.path.join(results_path,country+bin_string+'.csv')
         if os.path.isfile(file_path):
             print(f'{file_path} already exists. Delete file to run this set.')
             continue
             
+        # Generate the job submission string to be submitted to Euler
         bash_str = f'bsub -n {cores} -W {wall_time} -J "{country}-{i}" -oo {results_path}/lsf.{country}-{i}.txt '\
             f'python {SAF_directory}/scripts/optimization/plant_optimization.py '\
             f'--SAF_directory {SAF_directory} '\
@@ -105,5 +118,7 @@ for country in countries:
             f'--verbose '\
         # bash_str = f'python $HOME/EuroSAFs/scripts/03_plant_optimization/02_plant_optimization.py -d $HOME/EuroSAFs -c {country} -m {MIPGap} -i {DisplayInterval} -b {i} -n {bin_size} -v -s'
         # bash_str = f'python $HOME/GitHub/EuroSAFs/scripts/03_plant_optimization/02_plant_optimization.py -d $HOME/GitHub/EuroSAFs -c {country} -m {MIPGap} -i {DisplayInterval} -b {i} -n {bin_size} -v -s'
+        
+        # Execute the generated job submission bash command 
         os.system(bash_str)
         time.sleep(0.1)
